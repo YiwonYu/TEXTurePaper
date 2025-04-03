15514514f230b50787dbb92d6efc1516c817a170
YiwonYu <2won.dev@gmail.com>
2025-04-03 17:25:50 +0900

    [test file add] test obj, config added

--- configs/text_guided/run_all.sh
+++ configs/text_guided/run_all.sh
@@ -1,26 +1,35 @@
 #!/bin/bash 
 # command : bash configs/text_guided/run_all.sh
 # List of YAML files
-configs=(
-    "alien_1.yaml"
-    "alien_2.yaml"
-    "alien_3.yaml"
-    "alien_4.yaml"
-    "alien_5.yaml"
-    "rabbit_1.yaml"
-    "rabbit_2.yaml"
-    "rabbit_3.yaml"
-    "rabbit_4.yaml"
-    "rabbit_5.yaml"
-    "sphere_1.yaml"
-    "sphere_2.yaml"
-    "sphere_3.yaml"
-    "sphere_4.yaml"
-    "sphere_5.yaml"
-)
+# configs=(
+#     "alien_1.yaml"
+#     "alien_2.yaml"
+#     "alien_3.yaml"
+#     "alien_4.yaml"
+#     "alien_5.yaml"
+#     "rabbit_1.yaml"
+#     "rabbit_2.yaml"
+#     "rabbit_3.yaml"
+#     "rabbit_4.yaml"
+#     "rabbit_5.yaml"
+#     "sphere_1.yaml"
+#     "sphere_2.yaml"
+#     "sphere_3.yaml"
+#     "sphere_4.yaml"
+#     "sphere_5.yaml"
+# )
 
-# Loop through each config and run the command
-for config in "${configs[@]}"
+# # Loop through each config and run the command
+# for config in "${configs[@]}"
+# do
+#     python -m scripts.run_texture --config_path=configs/text_guided/$config
+# done
+
+#!/bin/bash 
+# command: bash configs/text_guided/run_all.sh
+
+# Loop through each YAML file in the text_guided folder
+for config in configs/text_guided/*.yaml
 do
-    python -m scripts.run_texture --config_path=configs/text_guided/$config
+    python -m scripts.run_texture --config_path="$config"
 done
